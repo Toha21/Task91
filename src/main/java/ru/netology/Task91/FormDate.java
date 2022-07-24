2668b120637a7a4e73ba2e6a477d524bc0711e2e
Anton Gavryukhin <ah21@mail.ru>
2022-07-24 19:10:16 +0300

    First commit

---
+++ src/main/java/ru/netology/Task91/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.Task91;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/Task91/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.Task91;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;// поле паспорт
+    public String patronymic; // поле для отчество
+    public String phone;// поле номер телефона
+    public String surname; // поле для фамилии
+    public boolean subscription;// поле подписки
+
+    public FormDate birthday;
+}
